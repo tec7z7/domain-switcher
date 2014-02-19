2b9ba1514db52e5e52f3df0d622c1fd3e63df56f
tec7z7 <jared@dsprinting.com>
2014-02-19 14:03:02 -0600

    Initial commit.

---
+++ options-script.js
@@ -0,0 +1,50 @@
+var envs = localStorage.getItem('envs');
+envs = JSON.parse(envs);
+$(document).ready(function() {
+
+	if(envs) {
+		var count = 1;
+		for (var key in envs) {
+			(function(key) {
+				var element = "<input type='text' data-oldName='"+key+"' id='newName"+count+"' value='"+key+"' /> ";
+				element += "<input type='text' data-oldAddress='"+envs[key]+"' id='newAddress"+count+"' value='"+envs[key]+"' /> ";
+				element += "<button class='update' id='"+count+"'>Update</button> ";
+				element += "<button class='delete' id='"+key+"' >Delete</button><br>";
+				$("#envsList").append(element);
+				count++;
+			}(key));
+		}
+	}
+
+	$(".update").click(function() {
+		var id = $(this).attr('id');
+		var NN = $("#newName"+id).val();
+		var NA = $("#newAddress"+id).val();
+		var ON = $("#newName"+id).attr('data-oldName');
+		var OA = $("#newAddress"+id).attr('data-oldAddress');
+		delete envs[ON];
+		envs[NN] = NA;
+		localStorage.setItem('envs', JSON.stringify(envs));
+		alert(NN+': '+NA+' updated.');
+		location.reload();
+	});
+
+	$(".delete").click(function() {
+		var id = $(this).attr('id');
+		delete envs[id];
+		localStorage.setItem('envs', JSON.stringify(envs));
+		location.reload();
+	});
+
+	$("#add").click(function() {
+		var AN = $("#addName").val();
+		var AA = $("#addAddress").val();
+		if(AN == '') {alert("Name cannot be blank."); return false;}
+		if(AA == '') {alert("Address cannot be blank."); return false;}
+		if(!AN.match('/^[a-z0-9]+$/i')) {alert('Name must be alphanumeric.'); return false; }
+		envs[AN] = AA;
+		localStorage.setItem('envs', JSON.stringify(envs));
+		location.reload();
+	});
+
+});
---
+++ script.js
@@ -0,0 +1,53 @@
+//Creates default environments if none are set
+if(!localStorage.getItem('envs')) {
+	var envs = {
+		'dev': 'http://vitals.dev',
+		'qa': 'http://qa.mdxdev.net',
+		'staging': 'http://staging.vitals.com',
+		'production': 'http://www.vitals.com'
+	};
+	localStorage.setItem('envs', JSON.stringify(envs));
+}
+
+//Retrieve stored environments
+var envs = localStorage.getItem('envs');
+envs = JSON.parse(envs);
+
+//Get current tab url, remove http:// protocol to get domain
+//remove domain, append path to the passed in new domain
+function changeDomain(newEnv) {
+	chrome.tabs.getSelected(null, function(tab){
+		var m = tab.url.match(/^http:\/\/[^/]+/);
+		var domain = m ? m[0] : '';
+		var path = tab.url.replace(domain, '');
+		chrome.tabs.update(tab.id, {url: newEnv+path});
+	});
+}
+
+$(document).ready(function() {
+
+	//Loop through stored environments and create div elements and append to #envsList div
+	for (var key in envs) {
+		(function(key) {
+			var item = key.charAt(0).toUpperCase() + key.slice(1);
+			$("#envsList").append("<div id='"+key+"'>"+item+"</div>");
+		}(key));
+	}
+
+	//Bind click function to dynamically created divs
+	for (var key in envs) {
+		(function(key) {
+			$('#'+key).click(function() {
+				changeDomain(envs[key]);
+			});
+		}(key));
+	}
+
+	//Open options.html in new tab
+	$("#options").click(function() {
+		chrome.tabs.create({'url': chrome.extension.getURL('options.html')}, function(tab) {
+			// Tab opened.
+		});
+	});
+
+});
